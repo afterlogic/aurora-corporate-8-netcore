10f9665543315e6262a0bf64a4135e3ffa90913a
Jarweb01 <jarvisweb@mail.ru>
2024-09-12 17:21:28 +0300

    Added VerticalTabsbarPinButton module

---
+++ _modules_diff/modules/VerticalTabsbarPinButton/js/manager.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = function () {
+  return {
+    start: function () {
+      setTimeout(function () {
+        const tabsbarContent = $('.screens > .tabsbar > .content');
+        const tabsbar = $('.screens > .tabsbar');
+
+        if (tabsbarContent.length === 0 || tabsbar.length === 0) {
+          console.error('Could not find elements tabsbar or tabsbar content');
+          return;
+        }
+
+        function updateTabsbarClass() {
+          const isExpanded = localStorage.getItem('aurora_tabsbar_expanded') === 'true';
+          tabsbar.toggleClass('pinned-tabs-bar', isExpanded);
+        }
+
+        if (localStorage.getItem('aurora_tabsbar_expanded') === null) {
+          localStorage.setItem('aurora_tabsbar_expanded', 'false');
+        }
+
+        updateTabsbarClass();
+
+        const newEl = $('<div>', { class: 'pin-menu' });
+        newEl.on('click', function () {
+          const currentState = localStorage.getItem('aurora_tabsbar_expanded') === 'true';
+          localStorage.setItem('aurora_tabsbar_expanded', !currentState);
+          updateTabsbarClass();
+        });
+        tabsbarContent.append(newEl);
+      }, 0);
+    },
+  };
+};
