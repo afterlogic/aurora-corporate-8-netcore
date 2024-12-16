e6dfcbe01df56d8728e219cc9f8d60fe61deff6b
Vasil Sokolov <vasil.sokolov@gmail.com>
2024-12-16 11:23:49 +0200

    Added user info tooltip for UserMenu

--- _modules_diff/modules/UserProfileMenu/js/views/HeaderItemView.js
+++ _modules_diff/modules/UserProfileMenu/js/views/HeaderItemView.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const _ = require('underscore')
+const ko = require('knockout')
 const TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js')
 const App = require('%PathToCoreWebclientModule%/js/App.js')
 const Settings = require('modules/SettingsWebclient/js/Settings.js')
@@ -11,35 +12,32 @@ const CAbstractHeaderItemView = require('%PathToCoreWebclientModule%/js/views/CH
 
 const accountList = ModulesManager.run('MailWebclient', 'getAccountList')
 
-const getInitials = () => {
+const getInitials = (email, name) => {
+  let initials = ''
   try {
-    const currentAccount = accountList.getCurrent()
-    const friendlyName = currentAccount.friendlyName()
-
-    if (friendlyName) {
-      const words = friendlyName.split(' ')
-
-      if (words.length >= 2) {
-        return words[0].charAt(0) + words[1].charAt(0)
-      } else if (words.length === 1) {
-        return words[0].slice(0, 2)
-      }
+    const words = name ? name.split(' ') : email.split('.')
+    
+    if (words.length >= 2) {
+      initials = words[0].charAt(0) + words[1].charAt(0)
+    } else if (words.length === 1) {
+      initials = words[0].slice(0, 2)
     }
+  } catch (error) {}
 
-    return currentAccount.email().slice(0, 2)
-  } catch (error) {
-    return ''
-  }
+  return initials
 }
 
 function CHeaderItemView() {
   CAbstractHeaderItemView.call(this, TextUtils.i18n('COREWEBCLIENT/LABEL_USER_MENU'))
 
-  accountList.currentId.subscribe(() => {
-    this.linkText(getInitials())
-  })
+  // accountList.currentId.subscribe(() => {
+  //   this.linkText()
+  // })
 
-  this.linkText(getInitials())
+  // this.linkText(getInitials())
+
+  this.userName = ko.observable('')
+  this.userEmail = ko.observable('')
 
   this.items = [
     {
@@ -55,6 +53,14 @@ function CHeaderItemView() {
         clickEvent: App.logout.bind(App)
     }
   ]
+
+  ko.computed(function () {
+    const currentAccount = accountList.getCurrent()
+    const initials = getInitials(currentAccount.email(), currentAccount.friendlyName())
+    this.linkText(initials)
+    this.userName(currentAccount.friendlyName())
+    this.userEmail(currentAccount.email())
+  }, this)
 }
 
 _.extendOwn(CHeaderItemView.prototype, CAbstractHeaderItemView.prototype)
